4a171385fcd903de17cf422005bc4b775d4125d9
Cathryn Ploehn <ploehn.cathryn@gmail.com>
2018-11-11 16:24:36 -0500

    Added scale and custom line characteristic input

--- sketch.js
+++ sketch.js
@@ -17,6 +17,35 @@
 	var destination;
 	var origin;
 	var dest;
+	var stroke = 1;
+	var opacity = 1;
+
+	
+	var tensionScale =  d3.scaleLinear()
+								.domain([0,1])
+								.range([1, 0]);
+	
+	var heightScale =  d3.scaleLinear()
+								.domain([0,1])
+								.range([10, 30]);
+
+	var waveScale =  d3.scaleLinear()
+								.domain([0,1])
+								.range([17, 30]);
+
+	var vNoiseScale =  d3.scaleLinear()
+								.domain([0,1])
+								.range([1, 5]);
+	var hNoiseScale =  d3.scaleLinear()
+								.domain([0,1])
+								.range([1, 1]);
+	var strokeScale = d3.scaleLinear()
+								.domain([0,1])
+								.range([5, 10]);
+	var opacityScale = d3.scaleLinear()
+								.domain([0,1])
+								.range([0.25, 1]);
+
 
 
 	function parseResult() {
@@ -31,10 +60,12 @@
 	}
 
 	function respond (){
-		aiSpeaking = true;
-		counselor.speak(transcript[currentLine].response);	
 		updateLine();		
-		currentLine = currentLine < transcript.length - 1? currentLine + 1 : currentLine;
+		window.setTimeout(function(){
+			aiSpeaking = true;
+			counselor.speak(transcript[currentLine].response);	
+			currentLine = currentLine < transcript.length - 1? currentLine + 1 : currentLine;
+		},2000);
 	}
   
   window.onload = async function() {
@@ -103,11 +134,13 @@
 
 		
 		function drawLine(){
+			var segmnts = [];
 			var numSegs = transcript.length;
 			var width = paper.view.size.width/2;
 			var lineData = [{x: 0, y:0}];
 
 			var i = 0;
+			var line = 0;
 			while( i < ((numSegs) * 2)){
 				var empty1 = drawEmptySegment(i);
 				i++;
@@ -115,17 +148,19 @@
 				i++;
 				lineData = lineData.concat(empty1);
 				lineData = lineData.concat(segs);
+				line++;
 			}
 			var empty3 = drawEmptySegment(i+2);
 			lineData = lineData.concat(empty3);
 			console.log(lineData);
 
-				var tension = 0;
+				var tension = 1;
 			function drawSegment(i) {
-				var waveLength = width / 7;
-				var height = 30;
-				var vNoise = 5;
-				var hNoise = 5;
+				var waveLength = width / waveScale(transcript[line].valence);
+
+				var height = heightScale(transcript[line].expression);
+				var vNoise = vNoiseScale(transcript[line].expression);
+				var hNoise = 1;
 				
 				var numDots = Math.floor(width/waveLength - 2);
 				var j = i;
@@ -151,22 +186,41 @@
 
 			function drawEmptySegment(i){
 				var output = {x: i * width, y: 0 };
-				console.log(output);
 				return {x: i * width, y: 0 };
 			}
-
+			// for(i in lineData){
+			// 	var j = Math.floor((parseInt(i))/2) + 1;
+			// 	var tension = transcript[j]? tensionScale(transcript[j].valence): 0;
+			// 	var lineGenerator = d3.line()
+			// 		.x(function(data, index){return data.x;})
+			// 		.y(function(data, index){return data.y;})
+			// 		.curve(d3.curveCardinal.tension(tension));
+			// 	// console.log(segmnts);
+			// 	var line = lineGenerator([lineData[i]]);
+			// 	var path = new paper.Path(line);
+			// 	console.log(line);
+			// 	segmnts.push(path);
+			// }
+			// console.log(segmnts);
+			// pathGenerated = new paper.Path(segs);
+			
 			var lineGenerator = d3.line()
 				.x(function(data, index){return data.x;})
 				.y(function(data, index){return data.y;})
 				.curve(d3.curveCardinal.tension(tension));
-
 			pathGenerated = new paper.Path(lineGenerator(lineData));
+
+
+			d3.select("body").append("svg")
+				.append("path")
+              .attr("d", lineGenerator(lineData));
+
 			segmentWidth = pathGenerated.bounds.width / (numSegs+1);
 			
 			var origin = new paper.Point(pathGenerated.bounds.leftCenter.x, paper.view.size.height/2);
 			var destination = new paper.Point((pathGenerated.bounds.width/(numSegs + 1)), paper.view.size.height/2);
 			
-			pathGenerated.strokeWidth = 5;
+			pathGenerated.strokeWidth = 1;
 			pathGenerated.strokeColor = {
 			    gradient: {
 			        stops: [['rgba(0,0,0,0)', 0], ['white', .45],['rgba(0,0,0,0)', .47]]
@@ -184,6 +238,9 @@
 		destination = pathGenerated.position.x;
 		origin =  pathGenerated.strokeColor.origin.x;
 		dest = pathGenerated.strokeColor.destination.x;
+		stroke = strokeScale(transcript[currentLine-1].familiarity);
+		console.log(strokeScale);
+		opacity = opacityScale(transcript[currentLine-1].familiarity);
 		console.log(speakerGradient.fillColor.gradient.stops[0].color.alpha);
 
 		paper.view.onFrame = function(event) {
@@ -202,9 +259,15 @@
 				var vectorDest = dest - pathGenerated.strokeColor.destination.x;
 				var vectorAmp = level - speakerGradient.fillColor.gradient.stops[0].color.alpha;
 
+				var vectorStroke = stroke - pathGenerated.strokeWidth;
+				var vectorOpacity = opacity - pathGenerated.strokeColor.gradient.stops[1].color.alpha;
+
 				pathGenerated.position.x += vector / 120;  
 				pathGenerated.strokeColor.origin.x += vectorOrigin / 4;
 				pathGenerated.strokeColor.destination.x += vectorDest / 4;
+
+				pathGenerated.strokeWidth += vectorStroke / 30;
+				pathGenerated.strokeColor.gradient.stops[1].color.alpha += vectorOpacity / 30;
 
 			}
 
@@ -215,6 +278,8 @@
   	destination = destination - ((segmentWidth));
 	origin =  origin + ((segmentWidth)/128);
 	dest = dest + ((segmentWidth)/128);
+	stroke = strokeScale(transcript[currentLine-1].familiarity);
+	opacity = opacityScale(transcript[currentLine-1].familiarity);
   }
 
 	window.addEventListener("click", function(event) {
